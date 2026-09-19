3294a5f57df862adbac7d69eb7ad5927c559d17c
Maria GG <mggrabwosky@gmail.com>
2026-09-19 11:49:21 +0200

    Ejercicio Resta

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// 1. Pedir dos numeros al usuario y  calcule la suma, la resta, la division y la multiplicacion de estos. 
+// Para producto no podemos usar el operador * . cON FUNCIONES
+let a = parseInt(prompt("Introduce un numero"));
+let b =parseInt(prompt("Introduce otro numero"));
+//SUMA
+const addNumbers =(a,b)=>{
+    let result =a +b;
+    return result;
+}
+//RESTA
+const abstractNumbers =(a,b)=>{
+    let result=a-b;
+    return result;
+}
